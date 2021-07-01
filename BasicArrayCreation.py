51403c3e3690bab28956450feb77b030bfbcb309
codeonblock <codeonblock@outlook.com>
2021-07-01 23:33:32 +0100

    Create BasicArrayCreation.py
    ndarray.ndim
    the number of axes (dimensions) of the array.
    
    ndarray.shape
    the dimensions of the array. This is a tuple of integers indicating the size of the array in each dimension. For a matrix with n rows and m columns, shape will be (n,m). The length of the shape tuple is therefore the number of axes, ndim.
    
    ndarray.size
    the total number of elements of the array. This is equal to the product of the elements of shape.
    
    ndarray.dtype
    an object describing the type of the elements in the array. One can create or specify dtype’s using standard Python types. Additionally NumPy provides types of its own. numpy.int32, numpy.int16, and numpy.float64 are some examples.
    
    ndarray.itemsize
    the size in bytes of each element of the array. For example, an array of elements of type float64 has itemsize 8 (=64/8), while one of type complex32 has itemsize 4 (=32/8). It is equivalent to ndarray.dtype.itemsize.
    
    ndarray.data
    the buffer containing the actual elements of the array. Normally, we won’t need to use this attribute because we will access the elements in an array using indexing facilities.

---
+++ BasicArrayCreation.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+a = np.arange(15).reshape(3,5)
+a
+
+
+# In[3]:
+
+
+a.shape 
+
+
+# In[4]:
+
+
+a.ndim
+
+
+# In[5]:
+
+
+a.dtype.name
+
+
+# In[6]:
+
+
+a.itemsize
+
+
+# In[7]:
+
+
+a.size
+
+
+# In[8]:
+
+
+type(a)
+
+
+# In[11]:
+
+
+b = np.array([6,7,8])
+b
+
+
+# In[12]:
+
+
+type(b)
+
+
+# In[ ]:
+
+
+
+
